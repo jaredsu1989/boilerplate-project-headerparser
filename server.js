0c318688571e1311fcd42e911b27ab39b4d20224
Jared Su <jaredsu1989@gmail.com>
2019-06-14 15:26:19 +1000

    Update server.js

--- server.js
+++ server.js
@@ -19,11 +19,15 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// Request Header Parser Microservice
+app.get("/api/whoami", function (req, res) {
+  console.log(req.headers);
+  res.json({
+    "ipaddress": req.headers["x-forwarded-for"],
+    "language": req.headers["accept-language"],
+    "software": req.headers["user-agent"]
+  });
 });
-
 
 
 // listen for requests :)
